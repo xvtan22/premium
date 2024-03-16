4ae25182947898bc8f9495de89cd32981b72e18d
xvtan22 <163534845+xvtan22@users.noreply.github.com>
2024-03-16 22:34:36 +0700

    Update ADMINx.lua

--- ADMINx.lua
+++ ADMINx.lua
@@ -2521,7 +2521,7 @@ end)
 ---
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "ADMIN | Durian Hub", HidePremium = false, IntroText = "ADMIN | Tan Dz Library", SaveConfig = true, ConfigFolder = "ADMIN"})
+local Window = OrionLib:MakeWindow({Name = "ADMIN Durian Hub", HidePremium = false, IntroText = "ADMIN Tan Dz Library", SaveConfig = true, ConfigFolder = "ADMIN"})
 ----------Tab-----------------------
 local W = Window:MakeTab({
     Name = "Thông Tin",
