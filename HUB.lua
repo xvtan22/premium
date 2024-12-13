ca8468f29457048415e35c9b82f122cc45e88f91
xvtan22 <163534845+xvtan22@users.noreply.github.com>
2024-12-13 12:52:14 +0700

    Update HUB.lua

--- HUB.lua
+++ HUB.lua
@@ -14,11 +14,11 @@ local gameId = game.PlaceId
 
 if gameId == 2753915549 or gameId == 4442272183 or gameId == 7449423635 then
     -- Blox Fruits
-    print("Detectado Blox Fruits, executando script...")
+    print("Detect Blox Fruits, exec script...")
     safeLoad("https://raw.githubusercontent.com/xvtan22/premium/refs/heads/main/main.lua")
 elseif gameId == 16732694052 then
     -- Fisch
-    print("Detectado Meme Sea, executando script...")
+    print("Detect Fisch, exec script...")
     safeLoad("https://raw.githubusercontent.com/xvtan22/premium/refs/heads/main/Fisch.lua")
 else
     print("Jogo não reconhecido. Nenhum script foi executado.")
