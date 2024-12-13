78048b5498cef6228fb2c5ed5ff9c4c6d21cb981
xvtan22 <163534845+xvtan22@users.noreply.github.com>
2024-12-13 12:49:11 +0700

    Add files via upload

---
+++ HUB.lua
@@ -0,0 +1,25 @@
+local function safeLoad(url)
+    local success, errorMsg = pcall(function()
+        loadstring(game:HttpGet(url))()
+    end)
+    
+    if success then
+        print("Script executado com sucesso!")
+    else
+        warn("Erro ao executar o script: " .. errorMsg)
+    end
+end
+
+local gameId = game.PlaceId
+
+if gameId == 2753915549 or gameId == 4442272183 or gameId == 7449423635 then
+    -- Blox Fruits
+    print("Detectado Blox Fruits, executando script...")
+    safeLoad("https://raw.githubusercontent.com/xvtan22/premium/refs/heads/main/main.lua")
+elseif gameId == 16732694052 then
+    -- Fisch
+    print("Detectado Meme Sea, executando script...")
+    safeLoad("https://raw.githubusercontent.com/xvtan22/premium/refs/heads/main/Fisch.lua")
+else
+    print("Jogo não reconhecido. Nenhum script foi executado.")
+end
