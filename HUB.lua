bd1b45f336e83e7a86b952536d71a0fe19451d08
xvtan22 <163534845+xvtan22@users.noreply.github.com>
2024-12-15 20:49:12 +0700

    Update HUB.lua

--- HUB.lua
+++ HUB.lua
@@ -1,25 +1,7 @@
-local function safeLoad(url)
-    local success, errorMsg = pcall(function()
-        loadstring(game:HttpGet(url))()
-    end)
-    
-    if success then
-        print("Script executado com sucesso!")
-    else
-        warn("Erro ao executar o script: " .. errorMsg)
-    end
-end
+getgenv().Team = "Pirates"
+getgenv().AutoLoad = false --Will Load Script On Server Hop
+getgenv().SlowLoadUi  = false
+getgenv().ForceUseSilentAimDashModifier = false --Force turn on silent aim , if error then executor problem
+getgenv().ForceUseWalkSpeedModifier = false --Force turn on Walk Speed Modifier , if error then executor problem
 
-local gameId = game.PlaceId
-
-if gameId == 2753915549 or gameId == 4442272183 or gameId == 7449423635 then
-    -- Blox Fruits
-    print("Detect Blox Fruits, exec script...")
-    safeLoad("loadstring(game:HttpGet("loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()"))()")
-elseif gameId == 16732694052 then
-    -- Fisch
-    print("Detect Fisch, exec script...")
-    safeLoad("https://raw.githubusercontent.com/xvtan22/premium/refs/heads/main/Fisch.lua")
-else
-    print("Jogo não reconhecido. Nenhum script foi executado.")
-end
+ loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
