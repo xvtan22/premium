b0941cc1028d34696a4901ac055eac1ef4ff0c1a
xvtan22 <163534845+xvtan22@users.noreply.github.com>
2024-12-15 20:55:41 +0700

    Update HUB.lua

--- HUB.lua
+++ HUB.lua
@@ -2,5 +2,5 @@ getgenv().Games = {
   [2753915549] = "loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()", -- Blox Fruit Sea 1
   [4442272183] = "loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()", -- Blox Fruit Sea 2
   [7449423635] = "loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()", -- Blox Fruit Sea 3
-  [16732694052] = "https://raw.githubusercontent.com/xvtan22/premium/refs/heads/main/Fisch.lua", -- Fisch
+  [16732694052] = "loadstring(game:HttpGet("https://raw.githubusercontent.com/xvtan22/premium/refs/heads/main/Fisch.lua"))()", -- Fisch
 }
