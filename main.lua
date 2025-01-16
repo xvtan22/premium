a0573e8b27e987d5318f77ea5a4003e608ebad99
xvtan22 <163534845+xvtan22@users.noreply.github.com>
2025-01-16 19:10:35 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,4 @@
----deobf By Adz
----deobf By Adz
----deobf By Adz
----deobf By Adz
+
 function setTeam(teamName)
     local args = {
         [1] = "SetTeam",
@@ -43,7 +40,7 @@ TextLabel.AnchorPoint = Vector2.new(0.5, 0.5)
 TextLabel.Position = UDim2.new(0.5, 0, -0.025, 0)
 TextLabel.Size = UDim2.new(0, 200, 0, 50)
 TextLabel.Font = Enum.Font.FredokaOne
-TextLabel.Text = "https://discord.gg/pGn6xsmfSg"
+TextLabel.Text = "https://discord.gg/67tG8nmaQX"
 TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.TextSize = 20.00
 
@@ -113,7 +110,7 @@ UIGradient.Color = ColorSequence.new{
 UIGradient.Parent = UIStroke
 
 T.Font = Enum.Font.GothamBold
-T.Text = "Lion Hub Find Fruit"
+T.Text = "Dino Hub Collect chest"
 T.TextColor3 = Color3.fromRGB(255, 255, 255)
 T.TextSize = 16.5
 T.TextYAlignment = Enum.TextYAlignment.Bottom
@@ -151,8 +148,8 @@ end
 local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/Main/Library.lua"))()
 
 NotificationLibrary:SendNotification("Warning", "Chờ xíu...", 10)
-NotificationLibrary:SendNotification("Warning", "cảm ơn vì dùng Lion Hub", 10)
-NotificationLibrary:SendNotification("Warning", "https://discord.gg/pGn6xsmfSg", 10)
+NotificationLibrary:SendNotification("Warning", "cảm ơn vì dùng Dino Hub", 10)
+NotificationLibrary:SendNotification("Warning", "https://discord.gg/67tG8nmaQX", 10)
 
 spawn(function()
     while wait() do
@@ -1206,3 +1203,91 @@ spawn(function()
         end
     end
 end)
+
+local MaxSpeed = 300 -- Studs per second 380 no flag but kick
+
+local LocalPlayer = game:GetService("Players").LocalPlayer
+local Locations = workspace._WorldOrigin.Locations
+
+local function getCharacter()
+    if not LocalPlayer.Character then
+        LocalPlayer.CharacterAdded:Wait()
+    end
+    LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+    return LocalPlayer.Character
+end
+
+local function DistanceFromPlrSort(ObjectList: table)
+    local RootPart = getCharacter().LowerTorso
+    table.sort(ObjectList, function(ChestA, ChestB)
+        local RootPos = RootPart.Position
+        local DistanceA = (RootPos - ChestA.Position).Magnitude
+        local DistanceB = (RootPos - ChestB.Position).Magnitude
+        return DistanceA < DistanceB
+    end)
+end
+
+local UncheckedChests = {}
+local FirstRun = true
+
+local function getChestsSorted()
+    if FirstRun then
+        FirstRun = false
+        local Objects = game:GetDescendants()
+        for i, Object in pairs(Objects) do
+            if Object.Name:find("Chest") and Object.ClassName == "Part" then
+                table.insert(UncheckedChests, Object)
+            end
+        end
+    end
+    local Chests = {}
+    for i, Chest in pairs(UncheckedChests) do
+        if Chest:FindFirstChild("TouchInterest") then
+            table.insert(Chests, Chest)
+        end
+    end
+    DistanceFromPlrSort(Chests)
+    return Chests
+end
+
+local function toggleNoclip(Toggle: boolean)
+    for i,v in pairs(getCharacter():GetChildren()) do
+        if v.ClassName == "Part" then
+            v.CanCollide = not Toggle
+        end
+    end
+end
+
+local function Teleport(Goal: CFrame, Speed)
+    if not Speed then
+        Speed = MaxSpeed
+    end
+    toggleNoclip(true)
+    local RootPart = getCharacter().HumanoidRootPart
+    local Magnitude = (RootPart.Position - Goal.Position).Magnitude
+
+    RootPart.CFrame = RootPart.CFrame
+    
+    while not (Magnitude < 1) do
+        local Direction = (Goal.Position - RootPart.Position).unit
+        RootPart.CFrame = RootPart.CFrame + Direction * (Speed * wait())
+        Magnitude = (RootPart.Position - Goal.Position).Magnitude
+    end
+    toggleNoclip(false)
+end
+
+local function main()
+    while wait() do
+        local Chests = getChestsSorted()
+        if #Chests > 0 then
+            Teleport(Chests[1].CFrame)
+        else
+            -- You can put serverhop here
+        end
+    end
+end
+
+wait = task.wait
+main()
+
+
