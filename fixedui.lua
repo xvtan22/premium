7f5a562ecab914528de24614dba534f9eea160a6
xvtan22 <163534845+xvtan22@users.noreply.github.com>
2025-03-21 22:28:50 +0700

    Update fixedui.lua

--- fixedui.lua
+++ fixedui.lua
@@ -1389,33 +1389,3 @@ if IKAI then
 		return uitab
 	end
 end
-
-      local Window = library:Window("Switch",[[Hub]],[[Version : Leak  ]],"100644623613900",Enum.KeyCode.RightControl)
-      local General_Tab = Window:AddTab("General",[[7040391851]])
-     local Quest_Tab = Window:AddTab("Quest & Item",[[9606626859]])
-
-    General_Tab:Label("Label Tile")
-  
-   General_Tab:Toggle("Toggle","9606294253",false,function()
-  end)
-
-   General_Tab:Button("Button",function()
-   end)
-   
-   AutoSet = {
-    "Set 1",
-    "Set 2",
-    "Set 3",
-    "Set 4"
-}
-General_Tab:Dropdown("Dropdown Test",AutoSet,function()
-end)
-    
-    local Tessss = General_Tab:LabelP("Check")
-    Tessss:Set("Test")
-    
-General_Tab:Slider("Slider",1,100,60,function()
-end)    
-    
-  General_Tab:Textbox("TextBox",function()  
-  end)
